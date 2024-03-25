ea50c946478dd64531c23334d2152bd633c1be5a
Natnael Fekadu <Natifekadu808@gmail.com>
2024-03-25 14:52:36 +0300

    Update Next.js to version 14.1.4 and add dynamic logo based on theme in Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,8 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
+import logo from "../public/lightLogo.png";
+// import dlogo from "../public/darkLogo.png";
+// import { useTheme } from "next-themes";
+// import { useEffect } from "react";
 
 const Logo = () => {
-  return <Link href="/">Home</Link>;
+  // const { theme, setTheme } = useTheme();
+
+  // const initialTheme = localStorage.getItem("theme") || "light"; // Get stored theme or default to light
+  // useEffect(() => {
+  //   setTheme(initialTheme); // Set initial theme based on local storage
+  // }, []);
+
+  return (
+    <Link href="/">
+      <Image
+        // src={theme === "dark" ? dlogo : logo}
+        src={logo}
+        alt="Logo"
+        width={54}
+        height={54}
+        priority={true}
+      />
+    </Link>
+  );
 };
 
 export default Logo;
